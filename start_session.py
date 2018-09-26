7ac2222cd6cfb67cb0fdfda995d7df0ab0ca8755
Jim Ladd <jladd@redhat.com>
2018-09-25 23:48:29 -0700

    Load creds from TOWERKIT_CREDENTIAL_FILE if set

--- start_session.py
+++ start_session.py
@@ -1,9 +1,12 @@
+import os
+
 from towerkit import api, config, utils, exceptions, WSClient
 from towerkit.tower.utils import check_related, delete_all, get_all, uses_sessions
 from towerkit.tower.utils import as_user as _as_user
 
 def start_session():
-    config.credentials = utils.load_credentials('config/credentials.yml')
+    cred_file = os.getenv('TOWERKIT_CREDENTIAL_FILE', 'config/credentials.yml')
+    config.credentials = utils.load_credentials(cred_file)
     root = api.Api()
     config.use_sessions = True
     root.load_session().get()
